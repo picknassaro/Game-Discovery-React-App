3c001f5990f8e9cae071214662d29e65d52ee660
Nick Passaro <nick.a.passaro@gmail.com>
2024-09-23 13:28:15 -0400

    Added nav, aside, and main for responsive layout using <Grid> and <GridItem> elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav main"`,
+          lg: `"nav nav"
+               "aside main"`,
+        }}
+      >
+        <GridItem gridArea="nav">nav</GridItem>
+        <Show above={"lg"}>
+          <GridItem gridArea="aside">aside</GridItem>
+        </Show>
+        <GridItem gridArea="main">main</GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
