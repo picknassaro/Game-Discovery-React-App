8a6ba288e238cbf1531694b4d96cc9f92b8eb052
Nick Passaro <nick.a.passaro@gmail.com>
2024-09-26 19:21:33 -0400

    Explained what the specific hooks are doing.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,9 @@
 import useData from "./useData";
 
+// useGames is basically just useData with the /games endpoint specified (oversimplification but good enough for a noob). But it also has to declare a type for the Game object that it will return. This type is used in the GameCard component to shape the props that it receives.
 const useGames = () => useData<Game>("/games");
 
+// The Game interface will be exported and used by the GameCard component.
 export interface Game {
   id: number;
   name: string;
@@ -9,6 +11,8 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
+
+// The Platform interface will be exported and used by the GameCard component as well.
 export interface Platform {
   id: number;
   name: string;
