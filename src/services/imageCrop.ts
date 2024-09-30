41b7aa51b2f8b61bdd97c2d519082cfb70b4714b
Nick Passaro <nick.a.passaro@gmail.com>
2024-09-30 14:00:53 -0400

    Added image for when game image is missing.

--- src/services/imageCrop.ts
+++ src/services/imageCrop.ts
@@ -1,8 +1,12 @@
+import gameImageMissing from "../assets/images/gameImageMissing.webp";
+
 const getCroppedImageUrl = (url: string) => {
   if (url) {
     const target = "media/";
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  } else {
+    return gameImageMissing;
   }
 };
 
