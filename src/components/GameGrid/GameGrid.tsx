d0702af4c967b6b7238f52ac7fad1e3473bb428c
Nick Passaro <nick.a.passaro@gmail.com>
2024-09-30 14:01:16 -0400

    Changed default sort

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { Heading, Show, SimpleGrid, Text } from "@chakra-ui/react";
+import { Heading, HStack, Show, SimpleGrid, Text } from "@chakra-ui/react";
 import useQueryController, { Game } from "../../hooks/useQueryController";
 import GameCard from "../GameCard/GameCard";
 import GameCardSkeleton from "../GameCardSkeleton/GameCardSkeleton";
@@ -22,8 +22,8 @@ const GameGrid = ({ selectedGenre }: GameGridProps) => {
   const [filteredPlatform, setFilteredPlatform] = useState<number | undefined>(
     undefined
   );
-  const [sortedBy, setSortedBy] = useState<string>("name");
-  const [sortByLabel, setSortByLabel] = useState<string>("A-Z");
+  const [sortedBy, setSortedBy] = useState<string>("-metacritic");
+  const [sortByLabel, setSortByLabel] = useState<string>("Highest Rated");
 
   const siteSupportedPlatforms = [
     "PC",
@@ -59,57 +59,72 @@ const GameGrid = ({ selectedGenre }: GameGridProps) => {
   return (
     <>
       <Show above="lg">
-        <Heading>Games</Heading>
+        <Heading margin="0 0 20px 20px">Games</Heading>
       </Show>
-      <QueryModSelector
-        queryModHeader="Results Per Page"
-        keepHeader={true}
-        headerOrder={2}
-        queryModValue={["20", "25", "30", "35", "40"]}
-        selectedValue={pageSize}
-        onSelect={(value) => setPageSize(Number(value as string))}
-        takeValue="string"
-      />
-      <QueryModSelector
-        queryModHeader="All Platforms"
-        keepHeader={false}
-        queryModKeys={filteredPlatformIds}
-        queryModValue={filteredPlatformNames}
-        selectedValue={
-          filteredPlatform !== undefined
-            ? filteredPlatformNames?.[
-                filteredPlatformIds?.indexOf(filteredPlatform)
-              ]
-            : "Platform"
-        }
-        onSelect={(value) => setFilteredPlatform(value as number)}
-        takeValue="index"
-      />
-      <QueryModSelector
-        queryModHeader="Sort By"
-        keepHeader={true}
-        headerOrder={1}
-        queryModKeys={[
-          "name",
-          "-name",
-          "released",
-          "-released",
-          "-metacritic",
-          "metacritic",
+      <HStack
+        width={[
+          "calc(100vw)",
+          "calc(100vw)",
+          "calc(100vw)",
+          "unset",
         ]}
-        queryModValue={[
-          "A-Z",
-          "Z-A",
-          "Newest",
-          "Oldest",
-          "Highest Rating",
-          "Lowest Rating",
-        ]}
-        selectedValue={sortByLabel}
-        onSelect={(value) => setSortedBy(value as string)}
-        onChangeLabel={(value) => setSortByLabel(value as string)}
-        takeValue="index"
-      />
+        display="flex"
+        alignItems="center"
+        whiteSpace="nowrap"
+        overflow="scroll"
+        padding="0 0 0 20px"
+        gap="0"
+      >
+        <QueryModSelector
+          queryModHeader="Results Per Page"
+          keepHeader={true}
+          headerOrder={2}
+          queryModValue={["20", "25", "30", "35", "40"]}
+          selectedValue={pageSize}
+          onSelect={(value) => setPageSize(Number(value as string))}
+          takeValue="string"
+        />
+        <QueryModSelector
+          queryModHeader="All Platforms"
+          keepHeader={false}
+          queryModKeys={filteredPlatformIds}
+          queryModValue={filteredPlatformNames}
+          selectedValue={
+            filteredPlatform !== undefined
+              ? filteredPlatformNames?.[
+                  filteredPlatformIds?.indexOf(filteredPlatform)
+                ]
+              : "Platform"
+          }
+          onSelect={(value) => setFilteredPlatform(value as number)}
+          takeValue="index"
+        />
+        <QueryModSelector
+          queryModHeader="Sort By"
+          keepHeader={true}
+          headerOrder={1}
+          queryModKeys={[
+            "name",
+            "-name",
+            "released",
+            "-released",
+            "-metacritic",
+            "metacritic",
+          ]}
+          queryModValue={[
+            "A-Z",
+            "Z-A",
+            "Newest",
+            "Oldest",
+            "Highest Rating",
+            "Lowest Rating",
+          ]}
+          selectedValue={sortByLabel}
+          onSelect={(value) => setSortedBy(value as string)}
+          onChangeLabel={(value) => setSortByLabel(value as string)}
+          takeValue="index"
+        />
+      </HStack>
       <SimpleGrid
         spacing="5"
         margin="0 20px"
