37bdb196e54f6dca00a96683cb0ae6d3bf9b25ef
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-02 10:56:32 -0400

    Temporarily removed local build directory stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, "../../build/www"),
-    emptyOutDir: true,
-  },
   base: "/Game-Discovery-React-App/",
 });
